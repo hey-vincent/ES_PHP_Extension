d410f1f4d32b742d8d0267be39bdd44c5ad1cf81
hey-vincent <heyvincent@126.com>
2017-11-19 20:53:08 +0800

    **

---
+++ ElasticSearchClient/test/excersize_book.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void Hello(){
+    cout << "Hello" << endl;
+}
+
+void hello(){
+    cout << "hello" << endl;
+}
+
+int main(){
+    Hello();
+    hello();
+}
